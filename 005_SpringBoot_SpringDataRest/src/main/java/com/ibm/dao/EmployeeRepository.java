8823224dd038ecdfc198c0392966aa35292736e0
INDRANEEL <javaindraneel@gmail.com>
2020-03-28 12:36:38 +0530

    First Commit

---
+++ 005_SpringBoot_SpringDataRest/src/main/java/com/ibm/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.ibm.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ibm.entity.Employee;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ 005_SpringBoot_SpringDataRest/src/main/java/com/ibm/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.ibm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="employee")
+@Data
+public class Employee {
+	
+	@Id
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+}
